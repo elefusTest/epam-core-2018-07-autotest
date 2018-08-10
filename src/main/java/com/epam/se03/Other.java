5a775caca2367e448fc698fd50da47fdc75fcd6c
Vyacheslav Lapin <Vyachesl@vLapin.ru>
2018-08-10 21:12:26 +0300

    strings - pooling

---
+++ src/main/java/com/epam/se03/Other.java
@@ -0,0 +1,24 @@
+package com.epam.se03;
+
+public final class Other {
+    static String hello = "Hello";
+
+    public static void main(String... __) {
+        String s1 = "Hello";
+        String s2 = new StringBuffer("He").append("llo").toString();
+        String s3 = s2.intern();
+
+        System.out.println("s1 == s2? " + (s1 == s2)); // false
+        System.out.println("s1 == s3? " + (s1 == s3)); // true
+
+        String hello = "Hello",
+                lo = "lo";
+
+        System.out.print((hello == "Hello") + " "); // true
+        System.out.print((Other.hello == hello) + " "); // true
+        System.out.print((com.epam.se03.otherpack.Other.hello == hello) + " "); // true
+        System.out.print((hello == ("Hel" + "lo")) + " "); // true
+        System.out.print((hello == ("Hel" + lo)) + " "); // false
+        System.out.println(hello == ("Hel" + lo).intern()); // true
+    }
+}
---
+++ src/main/java/com/epam/se03/otherpack/Other.java
@@ -0,0 +1,5 @@
+package com.epam.se03.otherpack;
+
+public final class Other {
+    public static String hello = "Hello";
+}
